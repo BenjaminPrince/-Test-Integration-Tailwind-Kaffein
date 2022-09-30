bb8bc9d9c6d1622bffb8b00f958d317c042ce1c0
BenjaminPrince <prince-benjamin@hotmail.com>
2022-09-30 14:23:05 +0200

    test tailwind integration Kaffein

---
+++ src/JS/index.js
@@ -0,0 +1,6 @@
+// Pour le burger de la navBar
+
+function Menu(e) {
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
+}
